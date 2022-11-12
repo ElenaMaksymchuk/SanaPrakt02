9dba0d575c776889922dad7c4e26fd7e35635c62
Elena Maksymchuk <sana22_mol@student.ztu.edu.ua>
2022-11-12 19:23:36 +0200

    2 last lines changed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,5 +3,5 @@ Console.WriteLine("1");
 Console.WriteLine("2");
 Console.WriteLine("3");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("*");
+Console.WriteLine("*");
